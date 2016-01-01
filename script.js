b961d47462b0698b1dad1eb32a37a4f50e76791c
Gal Bracha <galbra@gmail.com>
2016-01-01 15:04:51 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function() {
+	var datePickerOptions = {
+		format: 'MM/DD/YYYY'
+	};
+
+	$('#datetimepicker1').datetimepicker(datePickerOptions);
+
+	$('#datetimepicker2').datetimepicker(datePickerOptions);
+});
